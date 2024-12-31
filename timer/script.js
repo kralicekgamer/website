1d164a20132de51cdf42c5d47e5b30cdb92fe2dd
Matěj Adamec <kralicekgamer@gmail.com>
2024-12-31 21:24:56 +0100

    Added timer

---
+++ timer/script.js
@@ -0,0 +1,19 @@
+const countdown = () => {
+    const countDate = new Date('Jan 1, 2025 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+
+    const textHour = Math.floor(gap / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.getElementById('hours').innerText = textHour;
+    document.getElementById('minutes').innerText = textMinute;
+    document.getElementById('seconds').innerText = textSecond;
+};
+
+setInterval(countdown, 1000);
